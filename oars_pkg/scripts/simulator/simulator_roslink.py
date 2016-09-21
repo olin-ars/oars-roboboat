d8d9b021a7562c54266ef2a98c5f6f692bde0a1a
Eric Miller <eric@legoaces.org>
2016-09-21 19:09:16 -0400

    Changed topic names in simulator to remove sailing references

--- oars_pkg/scripts/simulator/simulator_roslink.py
+++ oars_pkg/scripts/simulator/simulator_roslink.py
@@ -17,20 +17,13 @@ class ROShandler():
         self.registerPubs()
 
     def registerSubs(self):
-        self.rudderSub = rospy.Subscriber('/rudder/set_point',
-                                          Int16, self.onRudder)
-        self.sailSub = rospy.Subscriber('/sail/set_point',
-                                        Float32, self.onSail)
+        self.rudderSub = rospy.Subscriber('/rudder_angle',
+                                          Float32, self.onRudder)
+        self.sailSub = rospy.Subscriber('/propeller_power',
+                                        Float32, self.onPower)
 
     def registerPubs(self):
         self.locationPub = rospy.Publisher('location', Pose2D)
-        self.velocityPub = rospy.Publisher('velocity', Pose2D)
-        self.true_windPub = rospy.Publisher('true_wind', Pose2D)
-        self.relative_windPub = rospy.Publisher('relative_wind', Pose2D)
-        self.global_windPub = rospy.Publisher('global_wind', Pose2D)
-
-        self.sailPub = rospy.Publisher('/sail/pos', Float32)
-        self.rudderPub = rospy.Publisher('/rudder/pos', Int16)
 
     def publish(self):
         def angleconvert(valin):
@@ -44,35 +37,17 @@ class ROShandler():
         b = model.boat1
         self.locationPub.publish(Pose2D(b.xpos, b.ypos, angleconvert(b.heading)))
 
-        # This nasty math creates a 0...359 degrees CW from North
-        vheading = angleconvert(math.atan2(b.vy, b.vx))
-        self.velocityPub.publish(Pose2D(math.sqrt(b.vx**2+b.vy**2), 0, vheading))
-
-        w = model.wind
-        self.true_windPub.publish(Pose2D(w.windspeed, 0, angleconvert(w.windheading-b.heading-math.pi/2)))
-        self.global_windPub.publish(Pose2D(w.windspeed, 0, angleconvert(w.windheading + math.pi)))
-
-        (wvx, wvy) = (w.windspeed * math.cos(w.windheading), w.windspeed * math.sin(w.windheading))
-        (wvx, wvy) = (wvx - b.vx, wvy - b.vy)
-
-        self.relative_windPub.publish(Pose2D(math.sqrt(wvx**2 + wvy**2), 0,
-                                      angleconvert(math.atan2(wvy, wvx) - b.heading + 3*math.pi/2)))
-
-        self.sailPub.publish(Float32(model.boat1.MainPos*6))
-        self.rudderPub.publish(Int16(model.boat1.RudderPos*90))
 
     def onRudder(self, msg):
         """The ROS network thinks in degrees,
         the sim things in 1/4th rotations"""
         self.model.boat1.RudderSuggestion = msg.data * 1/90.0
 
-    def onSail(self, msg):
-        """The ROS network thinks in sensors (0-6),
-        the sim things in 1/4th rotations"""
-        self.model.boat1.MainSuggestion = msg.data * 1.0/6
+    def onPower(self, msg):
+        self.model.boat1.motorboat = msg.data * 10
 
 if __name__ == '__main__':
-    model = sim.WorldModel(20, -math.pi/4)  # initial windspeed, the direction the wind is going (not coming from)
+    model = sim.WorldModel(0, 0) # disable wind in the model
     roshandler = ROShandler(model)
 
     r = rospy.Rate(10)
