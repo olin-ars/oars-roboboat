df18f1da0ce69aa6dce558da755996d81fecfa82
Eric Miller <eric@legoaces.org>
2016-09-21 18:52:51 -0400

    Created sample code for ROS subscribing and publishing

---
+++ oars_pkg/scripts/sample_code.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+import rospy
+
+# This line may need to change depending on your message types
+from std_msgs.msg import Int16, Float32
+
+class autonomousRudderPublisher:#needs a topic to read in waypoint angle from
+	def __init__(self):
+		rospy.init_node('rudder_publisher')
+
+		# Step 1: Initialize publishers. This will be referred to later whenever data is transmitted.
+		self.rudderPub = rospy.Publisher('auto_mode/rudder/set_point', Int16)
+
+		# Step 2: Initialize subscribers. Each subscriber (incomming information) gets a "self.*" variable, a callback, and a subscriber
+		self.waypoint = 0
+		def callback(angle):
+			self.waypoint = angle.data
+		self.waypointAngleSub = rospy.Subscriber('heading_err', Int16, callback) #needs somewhere to get waypoint
+
+	def calculateRudderPosition(self, waypointAngle):
+		# Do your main logic in a separate function for cleanliness reasons.
+
+		turnAngle = 0
+		if abs(waypointAngle) < 90:
+			turnAngle = waypointAngle / 2  
+		else:
+			if(waypointAngle > 0):
+				turnAngle = 45
+			else:
+				turnAngle = -45
+		return turnAngle
+
+	def run(self):
+		rate = rospy.Rate(10)  # 10hz refresh rate
+		while not rospy.is_shutdown():
+			rud = self.calculateRudderPosition(self.waypoint)
+			
+			# This is where the actual publishing happens
+			self.rudderPub.publish(rud)
+
+			rate.sleep()
+
+if __name__ == '__main__':
+	try:
+		handler = autonomousRudderPublisher()
+		handler.run()
+	except rospy.ROSInterruptException:
+		pass
