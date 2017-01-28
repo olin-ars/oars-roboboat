10cc94cbc21b05915ec050facc699c731833ec5a
Eric Miller <eric@legoaces.org>
2017-01-28 15:47:25 -0500

    Added documentation

--- oars_pkg/scripts/behavior_engine/tasks.py
+++ oars_pkg/scripts/behavior_engine/tasks.py
@@ -18,15 +18,23 @@ class Task(object):
         self.active = False
 
     def start(self, finishcallback):
+        """
+        Starts the task
+        :param finishcallback: A function that will be called when self.stop() is called
+        """
         self.active = True
         self.finishCallback = finishcallback
         print("\rTask {} starting\r".format(self.name))
 
     def stop(self):
+        """
+        Stops the task and calls the function passed to start()
+        """
         print "\rTask {} finished\r".format(self.name)
         if self.active:
             self.active = False
-            self.finishCallback()
+            if self.finishCallback:
+                self.finishCallback()
         else:
             print("\rWARNING: Task {} stop called on inactive task\r")
 
@@ -35,6 +43,12 @@ class TopicTask(Task):
     """A TopicTask is a task that writes to ROS topics when it starts and stops"""
 
     def __init__(self, name, activationTopic="", doneTopic=""):
+        """
+        :param activationTopic: The ROS topic name (of type std_msgs/Bool)
+            that this Task will publish to when it starts or stops
+        :param doneTopic: The ROS topic name (of type std_msgs/Bool) that this
+            task will stop when it recieves a "True" from
+        """
         super(TopicTask, self).__init__(name)
 
         self.activationTopic = activationTopic
@@ -50,11 +64,13 @@ class TopicTask(Task):
             self.stop()
 
     def start(self, finishcallback):
+        """ Sends a True message to activationTopic and starts the task """
         self.activationPub.publish(Bool(True))
 
         super(TopicTask, self).start(finishcallback)
 
     def stop(self):
+        """ Sends a False message to activationTopic and stops the task"""
         self.activationPub.publish(Bool(False))
 
         super(TopicTask, self).stop()
