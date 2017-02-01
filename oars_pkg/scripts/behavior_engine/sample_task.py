0ec7ac39297894409aebdccf4640ec1b4ecae0a3
Eric Miller <eric@legoaces.org>
2017-02-01 12:56:26 -0500

    Improved style and reusability of sample task code

--- oars_pkg/scripts/behavior_engine/sample_task.py
+++ oars_pkg/scripts/behavior_engine/sample_task.py
@@ -3,6 +3,7 @@
 import rospy, time
 from std_msgs.msg import Bool
 
+rospy.init_node('sample_task_node')
 
 class Main(object):
     """docstring for Main"""
@@ -22,9 +23,11 @@ class Main(object):
         else:
             self.running = False
 
+    def finishTask(self):
+        self.donePub.publish(Bool(True))
+
     def run(self):
-        rospy.init_node('sample_task_node')
-        rate = rospy.Rate(5)  # 5hz refresh rate
+        r = rospy.Rate(5)  # 5hz refresh rate
 
         while not rospy.is_shutdown():
             if self.running:
@@ -32,9 +35,9 @@ class Main(object):
                 print("I'm running! {}".format(self.i))
 
                 if self.i >= 20:
-                    self.donePub.publish(Bool(True))
+                    self.finishTask()
 
-            rate.sleep()
+            r.sleep()
 
 
 if __name__ == '__main__':
