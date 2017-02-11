dad2d962e5fc96c2fe3d24a6afd96ea1b3d9c4b7
Joseph Sutker <joseph.sutker@students.olin.edu>
2017-02-11 15:56:44 -0500

    fixes z rot joystick direction and adds estop and unestop (estart?)

--- oars_pkg/scripts/joystick/joy_control.py
+++ oars_pkg/scripts/joystick/joy_control.py
@@ -3,16 +3,18 @@
 import math
 import rospy
 from sensor_msgs.msg import Joy
+from std_msgs.msg import Bool
 from oars_arbiter.voter import Voter_full
 from oars_arbiter import createVote
 
 class JoyController(object):
 	def __init__(self):
-		self.control={'p/s':3 ,'f/a':4, 'spin':1}
-		self.switch={'estop':0, 'rc_override':1}
+		self.control={'p/s':3 ,'f/a':4, 'spin':0}
+		self.switch={'estop':1, 'unestop':0, 'rc_override':3}
 
 		rospy.init_node('joy_control')
 		rospy.Subscriber('/joy', Joy, self.joy_cb)
+		self.estop_pub = rospy.Publisher('/estop', Bool, queue_size=0)
 
 		self.voter=Voter_full()
 		self.direction = 0
@@ -23,10 +25,20 @@ class JoyController(object):
 		ps_cmd=msg.axes[self.control['p/s']]
 		fa_cmd=msg.axes[self.control['f/a']]
 		spin_cmd=msg.axes[self.control['spin']]
+		start_cmd=msg.buttons[self.switch['unestop']]
+		stop_cmd=msg.buttons[self.switch['estop']]
 		speed_01, dir_rad = self.cartesian_to_polar(fa_cmd, ps_cmd)
+
 		self.speed = speed_01*100
 		self.direction = round(dir_rad*50/math.pi)
 		self.rotation = round(spin_cmd*25)
+		self.toggle_estop(stop_cmd, start_cmd)
+
+	def toggle_estop(self, stop_cmd, start_cmd):
+		command = Bool()
+		command.data = bool(stop_cmd)
+		if stop_cmd or start_cmd:
+			self.estop_pub.publish(command)
 
 	def cartesian_to_polar(self, x, y):
 		r = math.sqrt(x**2. + y**2.)
