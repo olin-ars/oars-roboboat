59f976effed85da5c82359ae76bf29758688abda
Eric Miller <eric@legoaces.org>
2017-03-20 11:46:38 -0400

    Added code to control propulsion motor

--- arduino_code/tugboat_teensy/tugboat_teensy.ino
+++ arduino_code/tugboat_teensy/tugboat_teensy.ino
@@ -22,41 +22,69 @@
 #include <ros.h>
 #include <std_msgs/Float32.h>
 
+void handleFailsafes();
+void handleStatusLED();
+
 bool do_blink = true;
-long lastMessageMillis = 0;
+long lastMessageMillis = -1e5;
 
 ros::NodeHandle  nh;
 
-Servo servo;
+Servo rudder;
 
-void servo_cb( const std_msgs::Float32& cmd_msg){
+void rudder_cb( const std_msgs::Float32& cmd_msg){
   int angle = cmd_msg.data + 90;
-  servo.write(angle); //set servo angle, should be from 0-180
+  rudder.write(angle);
+  lastMessageMillis = millis();
+}
+ros::Subscriber<std_msgs::Float32> rudder_sub("/rudder_angle", rudder_cb);
+
+const int PROP_CENTER = 90;
+const float PROP_K = 10.0; // How many "degrees" the prop should respond for 1 newton input command
+
+Servo prop;
+void prop_cb( const std_msgs::Float32& cmd_msg){
+  int power = (cmd_msg.data * PROP_K) + PROP_CENTER;
+  prop.write(power); //set servo angle, should be from 0-180
   lastMessageMillis = millis();
 }
 
-
-ros::Subscriber<std_msgs::Float32> sub("/rudder_angle", servo_cb);
+ros::Subscriber<std_msgs::Float32> prop_sub("/propeller_power", prop_cb);
 
 void setup(){
+  // Enable the LED pin
   pinMode(13, OUTPUT);
 
   nh.initNode();
-  nh.subscribe(sub);
+  nh.subscribe(rudder_sub);
+  nh.subscribe(prop_sub);
   
-  servo.attach(9); //attach it to pin 9
+  rudder.attach(9); //Connect to the rudder pin
+  prop.attach(10);
+
 }
 
 void loop(){
-  handleStatusLED();
   nh.spinOnce();
+  handleFailsafes();
+  handleStatusLED();
   delay(1);
 }
 
-void handleStatusLED(){
-  const byte LED_PIN = 13;
+void handleFailsafes(){
   long t = millis();
-  bool do_blink = (t - lastMessageMillis) > 200;
+  bool estop = (t - lastMessageMillis) > 1000;
+  do_blink = estop;
+  if (estop){
+    prop.write(PROP_CENTER);
+    // TODO: disconnect from servo instead of 
+    // just writing a supposedly neutral power
+  }
+}
+
+void handleStatusLED(){
+  long t = millis();
+  const byte LED_PIN = 13;
   if (do_blink){
     digitalWrite(LED_PIN, (t % 400) < 200);
   }else{
@@ -64,4 +92,3 @@ void handleStatusLED(){
   }
   
 }
-
