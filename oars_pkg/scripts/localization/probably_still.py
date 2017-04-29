c752e4ce7f87b25597db2ab5919ead96b73f42c7
Eric Miller <eric@legoaces.org>
2017-04-29 09:18:34 -0400

    Added reasonable things back into localization configuration

--- oars_pkg/scripts/localization/probably_still.py
+++ oars_pkg/scripts/localization/probably_still.py
@@ -1,7 +1,9 @@
+#!/usr/bin/env python
+
 import rospy
 from nav_msgs.msg import Odometry
 from std_msgs.msg import Header
-from geometry_msgs.msg import PoseWithCovariance, Pose, Vector3
+from geometry_msgs.msg import PoseWithCovariance, Pose, Vector3, Quaternion, TwistWithCovariance
 
 rospy.init_node('probably_still')
 
@@ -9,15 +11,32 @@ pub = rospy.Publisher('/probably_still', Odometry, queue_size=10)
 
 r = rospy.Rate(10)
 
-xy_variance = 1.0
-z_variance = 0.1
+xy_var = 1.0
+z_var = 0.1
+theta_xy_var = 0.5
+theta_z_var = 100
+
+vel_xy_var = 10.0
+vel_z_var = 0.5
+vel_theta_xy_var = 0.5
+vel_theta_z_var = 100
 
 msg = Odometry(
     header=Header(frame_id='map'),
-    pose=PoseWithCovariance(pose=Pose(position=Vector3(x=10)),
-                            covariance=[1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-                                        1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0,
-                                        0.0, 0.0, 0.0, 0.0, 0.0, 1.0])
+    pose=PoseWithCovariance(pose=Pose(position=Vector3(x=10), orientation=Quaternion(w=1)),
+                            covariance=[xy_var, 0.0, 0.0, 0.0, 0.0, 0.0,
+                                        0.0, xy_var, 0.0, 0.0, 0.0, 0.0,
+                                        0.0, 0.0, z_var, 0.0, 0.0, 0.0,
+                                        0.0, 0.0, 0.0, theta_xy_var, 0.0, 0.0,
+                                        0.0, 0.0, 0.0, 0.0, theta_xy_var, 0.0,
+                                        0.0, 0.0, 0.0, 0.0, 0.0, theta_z_var]),
+
+    twist=TwistWithCovariance(covariance=[vel_xy_var, 0.0, 0.0, 0.0, 0.0, 0.0,
+                                          0.0, vel_xy_var, 0.0, 0.0, 0.0, 0.0,
+                                          0.0, 0.0, vel_z_var, 0.0, 0.0, 0.0,
+                                          0.0, 0.0, 0.0, vel_theta_xy_var, 0.0, 0.0,
+                                          0.0, 0.0, 0.0, 0.0, vel_theta_xy_var, 0.0,
+                                          0.0, 0.0, 0.0, 0.0, 0.0, vel_theta_z_var])
 )
 
 while not rospy.is_shutdown():
